e9a785efc9b85cbcbf819cc785d355018914ef85
ShewiYT <71594547+ShewiYT@users.noreply.github.com>
2020-10-16 18:36:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,88 @@
+export const accounts = [
+    {
+        localId: 1,
+        id: 281856579,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=friends&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_user_id=281856579&sign=41fv7X974jY_bSxRSphNBCJ55K5e2zdLP7RGR8q73Ck&hashParams=%7B%22ref%22%3A%22281856579%22%7D",
+        role: 'stealer',
+        autoBuyImprovements: true,
+        transferUserId: 536282364,
+        isStorageMiddle: false,
+        keepInfected: false,
+        debug: true,
+    },
+    {
+        localId: 1,
+        id: 536282364,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=friends&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=desktop_web&vk_ref=other&vk_user_id=536282364&sign=8DF399YheYbMSx0RLjihuVlq1Nid4AsZAhgpghBfivw&hashParams=%7B%7D",
+        role: 'stealer',
+        autoBuyImprovements: false,
+        transferUserId: 281856579,
+        isStorageMiddle: true,
+        keepInfected: false,
+        debug: true,
+    },
+    {
+        localId: 2,
+        id: 123,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_iphone&vk_ref=other&vk_user_id=327421115&sign=f66knmw8zEpvPg3iFvjObhF9ISGrupaOoSXk_StN123",
+        role: 'infected',
+        autoBuyImprovements: false,
+        transferUserId: 123,
+        debug: true,
+    },
+    {
+        localId: 7,
+        id: 123,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_iphone&vk_ref=other&vk_user_id=368728786&sign=6Cu5Z925aO4nSTEJbTYqrhZefdKLrNp0H8be5MO5123",
+        role: 'stealer',
+        autoBuyImprovements: false,
+        transferUserId: 777,
+        debug: true,
+    },
+    {
+        localId: 7,
+        id: 123,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_iphone&vk_ref=other&vk_user_id=368728786&sign=6Cu5Z925aO4nSTEJbTYqrhZefdKLrNp0H8be5MO5123",
+        role: 'stealer',
+        autoBuyImprovements: false,
+        transferUserId: 777,
+        debug: true,
+    },
+    {
+        localId: 0,
+        id: 381871731,
+        wss: "wss://paper-scroll.ru/socket?vk_access_token_settings=&vk_app_id=7420483&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_iphone&vk_ref=other&vk_user_id=381871731&sign=kTwwWqoAPvuqkx3N428jPb6mn6hzwKC3Etsmcy4L3123",
+        role: 'miner',
+        autoBuyImprovements: true,
+        autoBuyBonuses: true,
+        transferUserId: 123,
+        debug: true,
+    },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
